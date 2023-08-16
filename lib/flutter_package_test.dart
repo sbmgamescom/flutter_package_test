1292b5fcbfe21015a631f93a75e339401b13e997
shakutov <shakutov_b@c.nis.edu.kz>
2023-08-16 11:43:12 +0600

    add

--- lib/flutter_package_test.dart
+++ lib/flutter_package_test.dart
@@ -1,7 +1,7 @@
 library flutter_package_test;
 
 /// A Calculator.
-class PerfulCalculator {
+class CustomCalculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
--- test/flutter_package_test_test.dart
+++ test/flutter_package_test_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_package_test/flutter_package_test.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = PerfulCalculator();
+    final calculator = CustomCalculator();
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
